b60b5f7a96fcc023bfaf86dadc644727b3a5ed51
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2022-10-10 09:51:44 +0100

    MathsAssist v0.2.0
    Signed-off-by: Leo Cooper <113992401+defineprogramming@users.noreply.github.com>

--- MathsAssist.py
+++ MathsAssist.py
@@ -1,31 +1,73 @@
 import time
+import os
+def clear():
+    os.system('cls')
 divider = "-------------------------"
+# Welcome
 print("========== MATHSASSIST STARTED ==========")
 time.sleep(1)
 print("Welcome to MathsAssist")
 print(divider)
+# Changelogs
 time.sleep(1)
-print("You're on Build 1 PRE-ALPHA")
+print("v0.2.0 changes:")
 print(divider)
-time.sleep(3)
-print("What would you like to do?")
+print("Added subtraction component")
+print("Added multiplication component")
+print("Added division component")
+print("Removed unneccessary lines")
+print("Removed response delays")
+print("Program now restarts after each calculation")
+print("Added error if response does not exist")
 print(divider)
-print("Options:")
-print("1 | Addition")
-print("2 | Subtraction")
-print("3 | Multiplication")
-print ("4 | Division")
-navigation = int(input("Enter a number: "))
-if navigation == 1:
-    time.sleep(1)
-    additionnuma = int(input("Enter number A: "))
-    time.sleep(1)
-    print("========== NUMBER A SUBMITTED ==========")
-    time.sleep(1)
-    additionnumb = int(input("Enter number B: "))
-    print("========== NUMBER B SUBMITTED ==========")
-    addition = additionnuma + additionnumb
-    time.sleep(1)
-    print("Result is", addition)
-    time.sleep(3)
-quit()
+# Main Program & Navigation Interface
+while True:
+    print("What would you like to do?")
+    print(divider)
+    print("Options:")
+    print("1 | Addition")
+    print("2 | Subtraction")
+    print("3 | Multiplication")
+    print("4 | Division")
+    navigation = int(input("Enter a number: "))
+    # Addition Module
+    if navigation == 1:
+        time.sleep(1)
+        additionnuma = int(input("Enter number A: "))
+        print("========== NUMBER A SUBMITTED ==========")
+        additionnumb = int(input("Enter number B: "))
+        print("========== NUMBER B SUBMITTED ==========")
+        addition = additionnuma + additionnumb
+        print("Result is", addition)
+    # Subtraction Module
+    if navigation == 2:
+        time.sleep(1)
+        subtractionnuma = int(input("Enter number A: "))
+        print("========== NUMBER A SUBMITTED ==========")
+        subtractionnumb = int(input("Enter number B: "))
+        print("========== NUMBER B SUBMITTED ==========")
+        subtraction = subtractionnuma - subtractionnumb
+        print("Result is", subtraction)
+    # Multiplication Module
+    if navigation == 3:
+        time.sleep(1)
+        multiplicationnuma = int(input("Enter number A: "))
+        print("========== NUMBER A SUBMITTED ==========")
+        multiplicationnumb = int(input("Enter number B: "))
+        print("========== NUMBER B SUBMITTED ==========")
+        multiplication = multiplicationnuma * multiplicationnumb
+        print("Result is", multiplication)
+    # Division Module
+    if navigation == 4:
+        time.sleep(1)
+        divisionnuma = int(input("Enter number A: "))
+        print("========== NUMBER A SUBMITTED ==========")
+        divisionnumb = int(input("Enter number B: "))
+        print("========== NUMBER B SUBMITTED ==========")
+        division = divisionnuma / divisionnumb
+        print("Result is", division)
+    # 404 Error Returned
+    else:
+        print("Sorry, the command you entered does not exist. (404)")
+        time.sleep(2)
+        clear()
